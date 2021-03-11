9c1aacb5e7643180961ecba7b18634ad9d3b2ffa
rent614 <rent31415@icloud.com>
2021-03-11 20:49:25 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-    validates :email,              presence: true
-    validates :encrypted_password, presence: true
+    
     validates :name,               presence: true
     validates :profile,            presence: true
     validates :occupation,         presence: true
