b4ea6bcbc0e26e9cf668a5d561e3e01a241c2720
rent614 <rent31415@icloud.com>
2021-03-11 12:10:14 +0900

    バリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+    validates :email,              presence: true
+    validates :encrypted_password, presence: true
+    validates :profile,            presence: true
+    validates :user_name,          presence: true
+    validates :team,               presence: true
+    validates :position,           presence: true
+
+end
