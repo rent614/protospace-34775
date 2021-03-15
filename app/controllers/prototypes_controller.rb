a56ded0f8d39acd45d94f3c7297a2efb3ae3217e
rent614 <rent31415@icloud.com>
2021-03-15 20:38:13 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,26 @@
 class PrototypesController < ApplicationController
   
+  def index
+  
+  end
 
   def new
-    @prototypes = Prototypes.new
+    @prototype = Prototype.new
   end
 
   def create
+    Prototype.create(prototype_params)
+    if current_user.create(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
-  def destroy
+  
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
